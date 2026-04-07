55017396ca4d2535a88d9516894fce48589b64ba
James Candido <james.candidof@gmail.com>
2026-04-07 10:40:26 -0300

    Delete Aula3_Logica_Par_ou_Impar_Pyhton/jogo.py
    testar pelo git

--- Aula3_Logica_Par_ou_Impar_Pyhton/jogo.py
+++
@@ -1,9 +0,0 @@
-jogador1 = int(input('informe um numero: '))
-jogador2 = int(input('informe um numero: '))
-
-numero = jogador1 + jogador2
-
-if(numero%2 == 0):
-    print(f"\nPar ganhou")
-else:
-    print(f"\nImpar ganhou")
